9c94a930c58e090f4879f5763d84120b0191a46d
Zurc <cruzjurado@gmail.com>
2024-10-31 11:16:11 +0000

    index port update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Configure CORS for development
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite's default port
+  origin: 'http://127.0.0.1:5173', // Vite's default port
   credentials: true
 }));
 
